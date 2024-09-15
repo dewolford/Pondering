ec1e342fe10d4fdfe2557443589ea07beb5776d7
Bkstone05 <bkstone42@tntech.edu>
2024-09-14 19:18:28 -0500

    Started Shops class needs alot more work

--- Shop.h
+++ Shop.h
@@ -1,1 +1,35 @@
+/************************
+ * Name:Shop.h
+ * Purpose: hold class and functions for object duckies
+ */
 
+#include<string> 
+#include<iomanip> 
+#include<iostream> 
+using namespace std;
+
+#include "duckclass.h"
+
+#ifndef Shop_h
+#define Shop_h
+
+    class Shop
+    {
+        private:
+        string shopName, location; 
+        int capacity, numOfDucks;
+        Duckies* ducksInShop;
+        int* numDucksShop;
+
+        public:
+
+        Shop(string a, string b, int c, int d)
+        {
+            shopName = a;
+            location = b;
+            capacity = c;
+            numOfDucks = d;
+        };
+
+    };
+#endif
